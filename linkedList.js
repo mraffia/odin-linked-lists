84adb85e9d7c6d7c570a5d6b68fb8dd34198c7b7
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-13 14:02:26 +0700

    Add prepend and append function to LinkedList class

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,27 @@
 class LinkedList {
 
-    constructor() {
+    constructor(head = null) {
+        this.head = head;
     }
   
+    prepend(value) {
+        let newNode = new Node(value, this.head);
+    }
+
+    append(value) {
+        if (head === null) {
+            this.prepend(value);
+        } else {
+            let tmp = head;
+            while (tmp.nextNode !== null) {
+                tmp = tmp.nextNode;
+            }
+
+            tmp.nextNode = new Node(value, null);
+        }
+    }
+
+    
 }
 
 class Node {
